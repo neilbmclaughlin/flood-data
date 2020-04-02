52a539e4f185d26d447b55665a650e7be57c5ec7
tedd mason <teddmason@gmail.com>
2020-04-02 12:03:53 +0100

    adding in some unit tests and integration tests for fwis api work

--- test/unit/functions/fwis-process.js
+++ test/unit/functions/fwis-process.js
@@ -3,50 +3,34 @@ const lab = exports.lab = Lab.script()
 const Code = require('@hapi/code')
 const handler = require('../../../lib/functions/fwis-process').handler
 const event = require('../../events/fwis-event.json')
-const Fwis = require('../../../lib/models/fwis')
 const wreck = require('../../../lib/helpers/wreck')
+const Fwis = require('../../../lib/models/fwis')
 
 // start up Sinon sandbox
 const sinon = require('sinon').createSandbox()
 
 lab.experiment('fwis processing', () => {
   lab.beforeEach(() => {
-
+    // setup mocks
+    sinon.stub(wreck, 'request').callsFake(() => {
+      return Promise.resolve({})
+    })
+    sinon.stub(Fwis.prototype, 'save').callsFake(() => {
+      return Promise.resolve({})
+    })
   })
   lab.afterEach(() => {
     // restore sinon sandbox
     sinon.restore()
   })
   lab.test('fwis process', async () => {
-    // setup mocks
-    sinon.stub(wreck, 'request').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Fwis.prototype, 'save').callsFake(() => {
-      return Promise.resolve({})
-    })
     await handler(event)
   })
 
   lab.test('fwis process S3 error', async () => {
-    // setup mocks
-    sinon.stub(wreck, 'request').callsFake(() => {
-      return Promise.reject(new Error('test error'))
-    })
-    sinon.stub(Fwis.prototype, 'save').callsFake(() => {
-      return Promise.resolve({})
-    })
-    await Code.expect(handler(event)).to.reject()
-  })
-
-  lab.test('fwis process fwis error', async () => {
-    // setup mocks
-    sinon.stub(wreck, 'request').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Fwis.prototype, 'save').callsFake(() => {
-      return Promise.reject(new Error('test error'))
-    })
+    wreck.request = () => {
+      return Promise.reject(new Error('Test error'))
+    }
     await Code.expect(handler(event)).to.reject()
   })
 })
